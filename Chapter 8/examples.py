db9e6a1ca5ac201da2d8a310a2ca61b71d723dc6
Hasnat Md. Abdullah <hasnatabdullah79@gmail.com>
2020-05-19 03:05:13 +0600

    chapter 8 folder, examples.py created

---
+++ Chapter 8/examples.py
@@ -0,0 +1,16 @@
+import os
+# myFiles=['accounts.txt','details.csv','invite.docx']
+# for filename in myFiles:
+#     print(os.path.join('/home/frankenstein',filename))
+
+# print(os.getcwd())
+# os.chdir('/home/frankenstein/Desktop')
+# print(os.getcwd())
+
+# os.makedirs('temp_directory')
+
+# totalSize=0
+# for filename in os.listdir('/home/frankenstein/Desktop'):
+#     print(os.path.join('/home/frankenstein/Desktop',filename))
+#     totalSize+=os.path.getsize(os.path.join('/home/frankenstein/Desktop',filename))
+# print(totalSize)
