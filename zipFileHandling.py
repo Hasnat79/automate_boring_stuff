88f1c16d5a090a31bd84a9381fdc5490d7c13fd4
Hasnat Md. Abdullah <hasnatabdullah79@gmail.com>
2020-05-21 22:03:45 +0600

    zip file handling with zipfile module

---
+++ zipFileHandling.py
@@ -0,0 +1,7 @@
+import zipfile,os
+os.chdir("/home/hasnat/Desktop/automate_boring_stuff/Chapter 9")
+exampleZip = zipfile.ZipFile('example.zip')
+print(exampleZip.namelist())
+spamInfo=exampleZip.getinfo('spam.txt')
+print(spamInfo.file_size)
+print(spamInfo.compress_size)
