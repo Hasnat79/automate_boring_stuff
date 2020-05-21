4e2c3cc7acb5d1efacb25b057b28fb84b6936bcd
Hasnat Md. Abdullah <hasnatabdullah79@gmail.com>
2020-05-21 23:53:01 +0600

    creating and adding zip files

--- zipFileHandling.py
+++ zipFileHandling.py
@@ -6,3 +6,8 @@ spamInfo=exampleZip.getinfo('spam.txt')
 print(spamInfo.file_size)
 print(spamInfo.compress_size)
 exampleZip.extractall('example_folder')
+
+# creating and adding to zip files
+newZip = zipfile.ZipFile('new.zip','w')
+newZip.write('delicious/spam.txt',compress_type=zipfile.ZIP_DEFLATED)
+newZip.close()
