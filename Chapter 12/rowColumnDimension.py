f10e5f09dbdb82045860e445b0aea44f78037af4
Hasnat Md. Abdullah <hasnatabdullah79@gmail.com>
2020-06-07 13:54:45 +0600

    dimension control of excel file

---
+++ Chapter 12/rowColumnDimension.py
@@ -0,0 +1,8 @@
+import openpyxl
+wb = openpyxl.Workbook()
+sheet=wb.active
+sheet['A1']='Tall row'
+sheet['B2']='Wide Column'
+sheet.row_dimensions[1].height=70
+sheet.column_dimensions['B'].width=20
+wb.save('dimension.xlsx')
