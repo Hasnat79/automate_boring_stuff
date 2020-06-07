438eedd32d36d0355644416fdc77f6241f1c2160
Hasnat Md. Abdullah <hasnatabdullah79@gmail.com>
2020-06-07 15:58:25 +0600

    decrypt an encrypted pdf

---
+++ Chapter 13/decryptPdf.py
@@ -0,0 +1,7 @@
+import PyPDF2
+pdfReader= PyPDF2.PdfFileReader(open('encrypted.pdf','rb'))
+print(pdfReader.isEncrypted)
+# pdfReader.getPage(0)
+print(pdfReader.decrypt('rosebud'))
+pageObj=pdfReader.getPage(0)
+print(pageObj)
